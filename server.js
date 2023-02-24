ed44975a66fedd3a6b12bf71edf834739483c4f5
sharedordaz <sharedordaz@live.com.mx>
2023-02-23 22:33:04 -0600

    Changed name

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ var http = require('http');
 
 http.createServer((request, response) => {
   response.writeHead(200, {'Content-Type': 'text/plain'});
-  let name = "Shared Ordaz";
-  response.write(`My name is ${name}`);
+  let name = "Jared Oaxaca";
+  response.write(`The secret name is ${name}`);
   response.end();
   
 }).listen(8080);
